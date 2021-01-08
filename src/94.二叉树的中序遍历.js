82250b6db3baceed55cac7b5eb739dcdbcea6b5d
IdevebI <idevebi@163.com>
2021-01-08 12:15:58 +0800

    94. inorder traversal

---
+++ src/94.二叉树的中序遍历.js
@@ -0,0 +1,129 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=javascript
+ *
+ * [94] 二叉树的中序遍历
+ *
+ * https://leetcode-cn.com/problems/binary-tree-inorder-traversal/description/
+ *
+ * algorithms
+ * Medium (74.53%)
+ * Likes:    817
+ * Dislikes: 0
+ * Total Accepted:    326.3K
+ * Total Submissions: 437.8K
+ * Testcase Example:  '[1,null,2,3]'
+ *
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ * 
+ * 
+ * 
+ * 示例 1：
+ * 
+ * 
+ * 输入：root = [1,null,2,3]
+ * 输出：[1,3,2]
+ * 
+ * 
+ * 示例 2：
+ * 
+ * 
+ * 输入：root = []
+ * 输出：[]
+ * 
+ * 
+ * 示例 3：
+ * 
+ * 
+ * 输入：root = [1]
+ * 输出：[1]
+ * 
+ * 
+ * 示例 4：
+ * 
+ * 
+ * 输入：root = [1,2]
+ * 输出：[2,1]
+ * 
+ * 
+ * 示例 5：
+ * 
+ * 
+ * 输入：root = [1,null,2]
+ * 输出：[1,2]
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 
+ * 
+ * 树中节点数目在范围 [0, 100] 内
+ * -100 
+ * 
+ * 
+ * 
+ * 
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ * 
+ */
+function TreeNode(val, left, right) {
+  this.val = (val === undefined ? 0 : val)
+  this.left = (left === undefined ? null : left)
+  this.right = (right === undefined ? null : right)
+}
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var inorderTraversal = function (root) {
+  let res = [];
+  let stack = [];
+  let node = root;
+
+  while (node || stack.length) {
+    // 压栈左子树节点
+    while (node) {
+      stack.push(node);
+      node = node.left;
+    }
+
+    // 出栈
+    node = stack.pop();
+    res.push(node.val);
+
+    node = node.right;
+  }
+
+  return res;
+};
+// @lc code=end
+
+var inorderTraversalRecursion = function (root) {
+  const res = [];
+
+  const inorder = (root) => {
+    if (!root) {
+      return;
+    }
+    // 左
+    inorder(root.left);
+    // 根
+    res.push(root.val);
+    // 右
+    inorder(root.right);
+  }
+
+  inorder(root);
+
+  return res;
+};
