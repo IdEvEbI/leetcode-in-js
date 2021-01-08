de77dd4b5b2944062064935c0462275f61337317
IdevebI <idevebi@163.com>
2021-01-08 18:58:56 +0800

    feat: 589. preorder dfs

---
+++ src/589.n叉树的前序遍历.js
@@ -0,0 +1,67 @@
+/*
+ * @lc app=leetcode.cn id=589 lang=javascript
+ *
+ * [589] N叉树的前序遍历
+ *
+ * https://leetcode-cn.com/problems/n-ary-tree-preorder-traversal/description/
+ *
+ * algorithms
+ * Easy (74.02%)
+ * Likes:    128
+ * Dislikes: 0
+ * Total Accepted:    60.7K
+ * Total Submissions: 82K
+ * Testcase Example:  '[1,null,3,2,4,null,5,6]'
+ *
+ * 给定一个 N 叉树，返回其节点值的前序遍历。
+ * 
+ * 例如，给定一个 3叉树 :
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 返回其前序遍历: [1,3,5,6,2,4]。
+ * 
+ * 
+ * 
+ * 说明: 递归法很简单，你可以使用迭代法完成此题吗?
+ */
+
+// @lc code=start
+/**
+ * // Definition for a Node.
+ * function Node(val, children) {
+ *    this.val = val;
+ *    this.children = children;
+ * };
+ */
+
+/**
+ * @param {Node} root
+ * @return {number[]}
+ */
+var preorder = function (root) {
+
+  const res = [];
+
+  const dfs = (node) => {
+    if (!node) {
+      return;
+    }
+
+    res.push(node.val);
+
+    for (const child of node.children) {
+      dfs(child);
+    }
+  }
+
+  dfs(root);
+
+  return res;
+};
+// @lc code=end
+
