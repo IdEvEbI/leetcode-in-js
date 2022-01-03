b98668a97dc9dc58dadeff1d89e280fcd4e359c4
IdevebI <idevebi@163.com>
2022-01-03 10:23:29 +0800

    feat: 1.两数之和

---
+++ src/1.两数之和.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+  const obj: {
+    [v: number]: number;
+  } = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    const v = nums[i]
+
+    if ((target - v) in obj) {
+      return [obj[target - v], i]
+    }
+    obj[v] = i
+  }
+
+  return []
+}
+// @lc code=end
+
+export { twoSum}
---
+++ test/1.两数之和.test.ts
@@ -0,0 +1,13 @@
+import { twoSum } from '../src/1.两数之和'
+
+test('[2, 7, 11, 15] 应该返回 [0, 1]', () => {
+  expect(twoSum([2, 7, 11, 15], 9)).toEqual([0, 1])
+})
+
+test('[3, 2, 4] 应该返回 [1, 2]', () => {
+  expect(twoSum([3, 2, 4], 6)).toEqual([1, 2])
+})
+
+test('[3, 3] 应该返回 [0, 1]', () => {
+  expect(twoSum([3, 3], 6)).toEqual([0, 1])
+})
